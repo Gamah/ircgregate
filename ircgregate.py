f53469d06eb4367808bc26184acf64ea17ba0bb1
Gamah <deadon22087@gmail.com>
2014-12-11 18:35:49 -0600

    whop whoop!
    integer inserts, users table, words table!

--- ircgregate.py
+++ ircgregate.py
@@ -1,9 +1,19 @@
+#!/usr/bin/env python
+
 import sys
 import socket
 import string
 import re
 import os
- 
+import pymysql
+
+conn = pymysql.connect(host='changeme', port=3306, user='changeme', passwd='changeme', db='ircgregate', autocommit=True)
+
+cur = conn.cursor()
+
+cur.execute("SELECT NULL")
+NULLCHECK = cur.fetchall()
+
 HOST = "irc.changeme.net"
 PORT = 6667
  
@@ -18,18 +28,42 @@ readbuffer = ""
  
 s=socket.socket( )
 s.connect((HOST, PORT))
- 
+
 s.send(bytes("NICK %s\r\n" % NICK, "UTF-8"))
 s.send(bytes("USER %s %s bla :%s\r\n" % (IDENT, HOST, REALNAME), "UTF-8"))
 #s.send(bytes("JOIN %s \r\n", % (CHANNEL) "UTF-8"));
-
+#deprecated
 def dbi(s,w):
-    if(s != "Jbot" and s !="statbot"):
+    if(s != "JBot" and s !="statbot"):
        # if(len(w) <= 50):
-        os.system("mysql --user=changeme --password=changeme -e \"INSERT INTO ircgregate.swagdata(user,word,timestamp) VALUES('%s','%s',now())\"" % (s, w))
-
+        cur.execute("INSERT INTO ircgregate.swagdata(user,word,timestamp) VALUES('%s','%s',now())" % (s, w))
+def dbni(u,w):
+    cur.execute("INSERT INTO transactions(idword,iduser,timestamp) VALUES('%s','%s',now())" %(w, u))
+#depricated soon
 def dbnw(w):
-        os.system("mysql --user=changeme --password=changeme -e \"INSERT INTO ircgregate.coolwords(word) VALUES('%s')\"" % (w))
+        cur.execute("INSERT INTO ircgregate.coolwords(word) VALUES('%s')" % (w))
+
+def dbwc(w):
+    cur.execute("SELECT wordCheck('%s')" % (w))
+    WORDID = cur.fetchall()
+    if(WORDID != NULLCHECK):
+        WORDID = WORDID[0]
+        print("word ", WORDID[0])
+        return WORDID[0]
+    else:
+        cur.execute("INSERT INTO words(word) VALUES('%s')" % (w))
+        return dbwc(w)
+
+def dbuc(u):
+    cur.execute("SELECT userCheck('%s')" % (u))
+    USERID = cur.fetchall()
+    if(USERID != NULLCHECK):
+        USERID = USERID[0]
+        print("user ", USERID[0])
+        return USERID[0]
+    else:
+        cur.execute("INSERT INTO users(user) VALUES('%s')" % (u))
+        return dbuc(u)
 
 def joinch(line):
     global CONNECTED
@@ -59,12 +93,15 @@ def getmsg(line):
 
 def getwords(line):
     user = getusr(line)
+    userID = dbuc(user)
     for word in getmsg(line).split():
         if(word[:7] == "http://" or word[:4] == "www." or word[:8] == "https://"):
             word = "hyperlink posted"
         else:
             word = re.sub(r'[\W_]+', '',word)
-        dbi(user,word)
+            wordID = dbwc(word)
+        #dbi(user,word)
+        dbni(userID,wordID)
 def newword(word):
         if(word[:7] != "http://"):
             dbnw(re.sub(r'[\W_]+', '',word))
@@ -78,7 +115,7 @@ while 1:
     for line in temp:
         line = str.rstrip(line)
         line = str.split(line)
-        #print(line)
+        print(line)
         if(line[0] == "PING"):
             s.send(bytes("PONG %s\r\n" % line[1], "UTF-8"))
         elif(CONNECTED == 0):
